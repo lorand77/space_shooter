37864a8cd27f1a68df65960b06e7781aacfecf86
lorand <lorand77@users.noreply.github.com>
2024-05-30 18:04:28 +0200

    Player

--- space_shooter.py
+++ space_shooter.py
@@ -13,6 +13,19 @@ screen = pygame.display.set_mode((WIDTH, HEIGHT))
 pygame.display.set_caption("Space Shooter")
 clock = pygame.time.Clock()
 
+class Player(pygame.sprite.Sprite):
+	def __init__(self):
+		pygame.sprite.Sprite.__init__(self)
+		self.image = pygame.Surface((50, 50))
+		self.image.fill((0, 255, 0))
+		self.rect = self.image.get_rect()
+		self.rect.centerx = WIDTH / 2
+		self.rect.bottom = HEIGHT - 30
+
+sprites_all = pygame.sprite.Group()
+player = Player()
+sprites_all.add(player)
+
 running = True
 while running:
 	for event in pygame.event.get():
@@ -20,6 +33,7 @@ while running:
 			running = False
 
 	screen.fill(BLACK)
+	sprites_all.draw(screen)
 	pygame.display.flip()
 
 	clock.tick(FPS)
