688ff716490b2955097d1cf948e160682f3392f7
lorand <lorand77@users.noreply.github.com>
2024-05-31 19:05:57 +0200

    Graphics

--- space_shooter.py
+++ space_shooter.py
@@ -1,23 +1,21 @@
 import pygame
-import random
 
 WIDTH = 480
 HEIGHT = 600
 FPS = 60
-
-# colors R  G  B
-BLACK = (0, 0, 0)
 
 pygame.init()
 screen = pygame.display.set_mode((WIDTH, HEIGHT))
 pygame.display.set_caption("Space Shooter")
 clock = pygame.time.Clock()
 
+background = pygame.image.load("assets/starfield.png").convert()
+
 class Player(pygame.sprite.Sprite):
 	def __init__(self):
 		pygame.sprite.Sprite.__init__(self)
-		self.image = pygame.Surface((50, 50))
-		self.image.fill((0, 255, 0))
+		self.image = pygame.image.load("assets/playerShip.png").convert_alpha()
+		self.image = pygame.transform.scale(self.image, (50, 38))
 		self.rect = self.image.get_rect()
 		self.rect.centerx = WIDTH / 2
 		self.rect.bottom = HEIGHT - 30
@@ -32,7 +30,7 @@ while running:
 		if event.type == pygame.QUIT:
 			running = False
 
-	screen.fill(BLACK)
+	screen.blit(background, background.get_rect())
 	sprites_all.draw(screen)
 	pygame.display.flip()
 
